f04b71bc89a71eeb2dca450918a18799c3ba942a
wleferrand <william@corefarm.com>
2011-05-26 11:04:55 +0200

    renaming compatibility function

--- lib/libptrace_stubs.c
+++ lib/libptrace_stubs.c
@@ -105,7 +105,7 @@ value ocaml_cont (value ocaml_pid, value ocaml_signal) {
   
   printf ("signal: %d\n", sig) ;
 
-  rc = ptrace (PT_CONTINUE, Int_val (ocaml_pid), addr, caml_convert_signal_number (sig)); 
+  rc = ptrace (PT_CONTINUE, Int_val (ocaml_pid), addr, convert_signal_number (sig)); 
 
   if (rc) {
       printf ("errno: %d\n", errno); 
